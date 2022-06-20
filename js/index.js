64fe944799cf7853572de13445d4841db30efcdd
Evgeniyilin <ievgeniy@live.ru>
2022-06-20 16:59:02 +0500

    less3.1_new

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-ReactDOM.render(
-    <div>
-        <h1>Привет!</h1>
-        <h3>Hello</h3>
-    </div>, document.getElementById("app"))
+// ReactDOM.render(<div><h1>Привет!</h1><h3>Hello</h3></div>, document.getElementById("dapp"))
+
+const inputClick = () => console.log("Clicked")
+const mouseOver = () => console.log("Mouse Over")
+
+const helpText = "Help text"
+
+const elements = (<div className="Name">
+    <h1>{helpText}</h1>
+    <input placeholder={helpText}
+        onClick={inputClick} onMouse={mouseOver} />
+    <p>{helpText == "Help text" ? "Yes" : "No"}</p>
+    </div>)
+
+const app = document.getElementById("dapp")
+
+ReactDOM.render(elements, app)
